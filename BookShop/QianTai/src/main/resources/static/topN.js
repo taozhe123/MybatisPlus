9af713f7e72e5b6c246b3c9d2a1a6c6a096c9cb3
woniumrwang <woniumrwang@qq.com>
2023-04-25 16:03:28 +0800

    前台  主页动态切换publicContent

---
+++ BookShop/QianTai/src/main/resources/static/topN.js
@@ -0,0 +1,19 @@
+
+
+  let topNVue = new Vue({
+    el: "#topN",
+    data() {
+        return {
+          N:9
+        }
+    },
+    methods: {
+
+    },
+    created() {
+      console.log("topNVue 对象创建完成了")
+
+    }
+
+  });
+
