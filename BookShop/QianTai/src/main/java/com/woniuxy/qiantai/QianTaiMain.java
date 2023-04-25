7441f588eefa533d7330dabf41f66a5d6b60de9f
woniumrwang <woniumrwang@qq.com>
2023-04-25 15:42:35 +0800

    前台  引入主页原型

---
+++ BookShop/QianTai/src/main/java/com/woniuxy/qiantai/QianTaiMain.java
@@ -0,0 +1,13 @@
+package com.woniuxy.qiantai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QianTaiMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QianTaiMain.class,args);
+    }
+
+}
