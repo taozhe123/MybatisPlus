3fa2d61c1395cef497d71a66b8efe537459a5201
woniumrwang <woniumrwang@qq.com>
2023-04-26 14:13:41 +0800

    前台  首页 图书类型列表

--- BookShop/QianTai/src/main/java/com/woniuxy/qiantai/controller/BooktypeController.java
+++ BookShop/QianTai/src/main/java/com/woniuxy/qiantai/controller/BooktypeController.java
@@ -1,9 +1,14 @@
 package com.woniuxy.qiantai.controller;
 
 
+import com.woniuxy.dal.entity.Booktype;
+import com.woniuxy.servicelayer.service.BooktypeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +21,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/booktype")
 public class BooktypeController {
+
+    @Autowired
+    BooktypeService booktypeService;
+
+
+    /**
+     * 查询所有图书类型
+     *
+     * @return
+     */
+    @RequestMapping("all")
+    public List<Booktype> all(){
+        return booktypeService.list();
+    }
+
+
 
 }
 
